073c0b63a91cf03cf5881f6702fe360234e21b3d
Daniel Andrus <danieleandrus@gmail.com>
2017-06-17 13:25:33 -0700

    Further refactors and cleanups.

--- VideoFrameManager.js
+++ VideoFrameManager.js
@@ -1,47 +1,44 @@
+/** Class for coordinating, building, and manaing video frames. */
 function VideoFrameManager(containerId) {
-  this.containerId = containerId;
+  this.frameContainerId = containerId;
   this.playerFrames = [];
 }
 
 /** Initializes the video frame manager. */
 VideoFrameManager.prototype.init = function() {
   this.showButtons();
-}
+};
 
 /** Replaces the contents of the container with buttons for layout options. */
 VideoFrameManager.prototype.showButtons = function() {
-  clearElementContents(this.getContainer());
+  
+clearElementContents(this.getContainer());
   this.getContainer().appendChild(this.createButtonForLayout(VideoLayouts[LayoutType.FULLSCREEN]));
   this.getContainer().appendChild(this.createButtonForLayout(VideoLayouts[LayoutType.VERTICAL_SPLIT]));
   this.getContainer().appendChild(this.createButtonForLayout(VideoLayouts[LayoutType.FOUR_CORNERS]));
-}
+};
 
 /** Gets the DOM element that is the container for all of the video frames. */
 VideoFrameManager.prototype.getContainer = function() {
-  return this.container == null
-      ? this.container = document.getElementById(this.containerId)
-      : this.container;
-}
+  return this.frameContainer || (this.frameContainer = document.getElementById(this.frameContainerId));
+};
 
 /** Creates a button that can be clicked by the user to load the given layout. */
 VideoFrameManager.prototype.createButtonForLayout = function(layout) {
-  var videoManager = this;
-  var button = document.createElement('button');
+  let button = document.createElement('button');
   button.textContent = layout.name;
-  button.onclick = function() {
-    videoManager.loadLayout(layout);
-  };
+  button.onclick = () => this.loadLayout(layout);
   return button;
-}
+};
 
 /** Update video frames to match the given layout. */
 VideoFrameManager.prototype.loadLayout = function(layout) {
   clearElementContents(this.getContainer());
   this.setNumberOfPlayers(layout.frames.length);
-  for (var i = 0; i < layout.frames.length; i++) {
+  for (let i = 0; i < layout.frames.length; i++) {
     setPlayerFrame(this.playerFrames[i], layout.frames[i]);
   }
-}
+};
 
 /** Create/destroy video frames until the number of frames match the given number. */
 VideoFrameManager.prototype.setNumberOfPlayers = function(numPlayers) {
@@ -54,46 +51,45 @@ VideoFrameManager.prototype.setNumberOfPlayers = function(numPlayers) {
   while (this.playerFrames.length > numPlayers) {
     this.popPlayer();
   }
-}
+};
 
 /** Create a new video player and add it to the DOM tree and the playerFrames array. */
 VideoFrameManager.prototype.pushPlayer = function() {
-  var player = this.createFrame();
+  let player = this.createFrame();
   this.getContainer().appendChild(player);
   this.playerFrames.push(player);
   return player;
-}
+};
 
 /** Remove the last video player from the DOM tree and the playerFrames array. */
 VideoFrameManager.prototype.popPlayer = function() {
-  var player = this.playerFrames.pop();
+  let player = this.playerFrames.pop();
   this.getContainer().removeChild(player);
   return player;
-}
+};
 
 /** Create a new video frame. */
 VideoFrameManager.prototype.createFrame = function() {
-  var videoManager = this;
-  var id = this.playerFrames.length;
+  let id = this.playerFrames.length;
   
-  var frame = document.createElement('div');
+  let frame = document.createElement('div');
   frame.classList.add('video');
   frame.id = id;
   
-  var form = document.createElement('form');
+  let form = document.createElement('form');
   form.name = 'form' + id;
   form.method = 'post';
-  form.onsubmit = function() {
-    videoManager.loadVideoOnFrame(frame, form.name);
+  form.onsubmit = () => {
+    this.loadVideoOnFrame(frame, form.name);
     return false;
   };
   
-  var textInput = document.createElement('input');
+  let textInput = document.createElement('input');
   textInput.type = 'text';
   textInput.name = 'video';
   textInput.placeholder = 'Video ID';
   
-  var submitInput = document.createElement('input');
+  let submitInput = document.createElement('input');
   submitInput.type = 'submit';
   submitInput.value = 'Go';
   
@@ -102,18 +98,18 @@ VideoFrameManager.prototype.createFrame = function() {
   form.appendChild(submitInput);
 
   return frame;
-}
+};
 
 /** Sets the content of a frame with that which is interpretted from the form with the given name. */
 VideoFrameManager.prototype.loadVideoOnFrame = function(frame, formName) {
-  var iframe = document.createElement('iframe');
+  let iframe = document.createElement('iframe');
   iframe.src = 'http://www.youtube.com/embed/' + document.forms[formName]['video'].value + '?rel=0';
   iframe.frameBorder = '0';
   iframe.setAttribute('allowfullscreen', '');
 
   clearElementContents(frame);
   frame.appendChild(iframe);
-}
+};
 
 
 /** Static function for removing all child elements from the given DOM element. */
