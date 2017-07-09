ee4ab98db2956439815c9f839b7995159a50c3d1
Daniel Andrus <danieleandrus@gmail.com>
2017-07-08 18:39:39 -0700

    Add usage instructions to video form.

--- VideoSelectionForm.js
+++ VideoSelectionForm.js
@@ -13,6 +13,15 @@ function VideoSelectionForm(id, onSubmit) {
     return false;
   };
 
+  let leadingInstructions = document.createElement('div');
+  leadingInstructions.classList.add('instructions');
+
+  let p1 = document.createElement('p');
+  p1.textContent = 'Paste a video URL, then enjoy the show!';
+  leadingInstructions.appendChild(p1);
+  
+  form.appendChild(leadingInstructions);
+
   let textInput = document.createElement('input');
   textInput.type = 'text';
   textInput.name = 'video';
@@ -23,6 +32,15 @@ function VideoSelectionForm(id, onSubmit) {
   submitInput.type = 'submit';
   submitInput.value = ' ';
   form.appendChild(submitInput);
+  
+  let followingInstructions = document.createElement('div');
+  followingInstructions.classList.add('instructions');
+  
+  let p2 = document.createElement('p');
+  p2.textContent = 'You can also drag and drop in a link.';
+  followingInstructions.appendChild(p2);
+  
+  form.appendChild(followingInstructions);
 
   this.element = form;
 }
