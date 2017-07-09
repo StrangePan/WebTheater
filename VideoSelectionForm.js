012c8c434c4983b039c174488ebe98cc9aa38904
Daniel Andrus <danieleandrus@gmail.com>
2017-07-08 18:09:27 -0700

    Update video input form styles.

--- VideoSelectionForm.js
+++ VideoSelectionForm.js
@@ -1,6 +1,7 @@
 function VideoSelectionForm(id, onSubmit) {
 
   let form = document.createElement('form');
+  form.classList.add('video-selection-form');
   form.name = 'form' + id;
   form.method = 'post';
   form.onsubmit = () => {
@@ -20,7 +21,7 @@ function VideoSelectionForm(id, onSubmit) {
 
   let submitInput = document.createElement('input');
   submitInput.type = 'submit';
-  submitInput.value = 'Go';
+  submitInput.value = ' ';
   form.appendChild(submitInput);
 
   this.element = form;
